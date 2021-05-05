00f7c8744911939db5e797a21e4c48c386fc9dc5
justinlachap <justinlachapel@gmail.com>
2021-05-05 17:05:03 -0400

    in search...

---
+++ CodeForces/Difficulty800/InSearchOfAnEasyProblem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--) {
+        int x;
+        cin >> x;
+        if (x) {
+            cout << "HARD";
+            return 0;
+        }
+    }
+    
+    cout << "EASY";
+}
