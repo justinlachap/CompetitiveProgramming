69a793c704a93b8b1d174d3bfdf192c7f84d14a3
justinlachap <justinlachapel@gmail.com>
2021-06-15 11:19:24 -0400

    done

---
+++ CodeForces/Difficulty900/ArrayReodering.py
@@ -0,0 +1,22 @@
+import math
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    evenA, oddA = [], []
+    for number in a:
+        if number % 2:
+            oddA.append(number)
+        else:
+            evenA.append(number)
+    a.clear()
+    a = sorted(evenA) + sorted(oddA)
+
+    count = 0
+    for j in range(len(a)):
+        for k in range(j + 1, len(a)):
+            if math.gcd(a[j], a[k] * 2) > 1:
+                count += 1
+    print(count)
